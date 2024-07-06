19489774352d5b54c46cf1dbb6c0a519a944561f
Nazirah01 <107243184+Nazirah01@users.noreply.github.com>
2024-07-06 12:30:01 +0800

    Update Car.java

--- Car/Car.java
+++ Car/Car.java
@@ -2,20 +2,33 @@ package com.example.lab_rest.model;
 
 public class Car {
     private int carID;
+    private int maintenance_ID;
+
+    private Maintenance maintenance;
 
     private String model;
 
+    private String brand;
+
+    private String plateNumber;
+
     private String availabilityStatus;
     private String remarks;
+    private String createdAt;
 
     public Car() {
     }
 
-    public Car(int carID, String model, String availabilityStatus, String remarks) {
+    public Car(int carID, int maintenance_ID, Maintenance maintenance, String model, String brand, String plateNumber, String availabilityStatus, String remarks, String createdAt) {
         this.carID = carID;
+        this.maintenance_ID = maintenance_ID;
+        this.maintenance = maintenance;
         this.model = model;
+        this.brand = brand;
+        this.plateNumber = plateNumber;
         this.availabilityStatus = availabilityStatus;
         this.remarks = remarks;
+        this.createdAt = createdAt;
     }
 
     public int getCarID() {
@@ -26,12 +39,44 @@ public class Car {
         this.carID = carID;
     }
 
+    public int getMaintenance_ID() {
+        return maintenance_ID;
+    }
+
+    public void setMaintenance_ID(int maintenance_ID) {
+        this.maintenance_ID = maintenance_ID;
+    }
+
+    public Maintenance getMaintenance() {
+        return maintenance;
+    }
+
+    public void setMaintenance(Maintenance maintenance) {
+        this.maintenance = maintenance;
+    }
+
     public String getModel() {
         return model;
     }
 
     public void setModel(String model) {
         this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
     }
 
     public String getAvailabilityStatus() {
@@ -50,13 +95,26 @@ public class Car {
         this.remarks = remarks;
     }
 
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
     @Override
     public String toString() {
         return "Car{" +
                 "carID=" + carID +
+                ", maintenance_ID=" + maintenance_ID +
+                ", maintenance=" + maintenance +
                 ", model='" + model + '\'' +
+                ", brand='" + brand + '\'' +
+                ", plateNumber='" + plateNumber + '\'' +
                 ", availabilityStatus='" + availabilityStatus + '\'' +
                 ", remarks='" + remarks + '\'' +
+                ", createdAt='" + createdAt + '\'' +
                 '}';
     }
 }
