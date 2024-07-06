fb939d7cc78c84524370085a4ef12c25f538949c
Nazirah01 <107243184+Nazirah01@users.noreply.github.com>
2024-07-06 14:11:30 +0800

    Create ApiUtils.java

---
+++ Car/ApiUtils.java
@@ -0,0 +1,30 @@
+package com.example.lab_rest.remote;
+
+import com.example.lab_rest.model.Maintenance;
+
+public class ApiUtils {
+    // REST API server URL
+    public static final String BASE_URL = "http://178.128.220.20/2023500191/api/";
+
+    //
+    // return UserService instance
+    public static UserService getUserService() {
+        return RetrofitClient.getClient(BASE_URL).create(UserService.class);
+    }
+
+    // return BookService instance
+    public static BookingService getBookingService() {
+        return RetrofitClient.getClient(BASE_URL).create(BookingService.class);
+    }
+
+    public static BookService getBookService() {
+        return RetrofitClient.getClient(BASE_URL).create(BookService.class);
+    }
+
+    public static CarService getCarService() {
+        return RetrofitClient.getClient(BASE_URL).create(CarService.class);
+    }
+    public static MaintenanceService getMaintenanceService() {
+        return RetrofitClient.getClient(BASE_URL).create(MaintenanceService.class);
+    }
+}
